fbbee78bee0b4528604fe9e55b2b277367bde644
jaymzsocool <james.mcraejr@gmail.com>
2018-09-26 18:37:11 -0400

    trying switch

--- src/App.js
+++ src/App.js
@@ -20,11 +20,13 @@ class App extends Component {
           <Route exact path = '/' render = {()=><Home />} />
           <Route exact path = '/register' render = {()=><Register />} />
           <Route exact path = '/login' render = {()=><Login />} />
-          <Route path = '/note' render = {()=><Sidebar />} />        
+          <Route path = '/note' render = {()=><Sidebar />} />
+        <Switch> 
           <Route exact path = '/note' render = {()=><NoteList />} />
           <Route exact path = '/note/view/:id' render = {()=><NoteView />} />
           <Route exact path = '/note/:id/edit' render = {()=><EditNote />} />
-          <Route exact path = '/note/new' render = {()=><AddNote />} />        
+          <Route exact path = '/note/new' render = {()=><AddNote />} />
+        </Switch>
       </div>
     );
   }
